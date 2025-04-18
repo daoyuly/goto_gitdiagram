de39a8740926a2dc9e7bae7702ead97a984bbcc0
Daoyu Liu <daoyu@umu.com>
2025-04-18 13:28:22 +0800

    first commit

---
+++ content.js
@@ -0,0 +1,12 @@
+(function(){
+    var ul = document.querySelector('header .AppHeader-globalBar .AppHeader-globalBar-start .AppHeader-context-full nav ul')
+    // ul 是否存在
+    if (!ul) {
+        return
+    }
+    var url = ul.innerText.replace(/\n/g, '')
+
+    var li = document.createElement('li')
+    li.innerHTML = '<a target="_blank" href="https://gitdiagram.com/'+url+'"><img title="goto diagram" style="width: 28px;" src="https://raw.githubusercontent.com/daoyuly/gitdiagram_link/refs/heads/main/icon-128.png"></a>'
+    ul.appendChild(li)
+})()
