1e474ed76cd11a7d1b2757f92f451c6f06ef2d7e
Daoyu Liu <daoyu@umu.com>
2025-04-18 13:44:04 +0800

    release v1.0

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -f goto_gitdiagram.zip
+rm -f ./dist/goto_gitdiagram.zip
 
-zip goto_gitdiagram.zip content.js manifest.json icon-32.png icon-128.png
+zip -r ./dist/goto_gitdiagram.zip content.js manifest.json icon-32.png icon-128.png
 
 echo "success: goto_gitdiagram.zip"
