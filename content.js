5377786cd71f07f824817d45b7da78eabc83eb78
Daoyu Liu <daoyu@umu.com>
2025-04-28 20:15:59 +0800

    update

--- content.js
+++ content.js
@@ -1,11 +1,18 @@
 (function(){
     var ul = document.querySelector('header .AppHeader-globalBar .AppHeader-globalBar-start .AppHeader-context-full nav ul');
-    if (!ul) {
-        return
+    if (ul) {
+        var url = ul.innerText.replace(/\n/g, '')
+        var li = document.createElement('li')
+        li.innerHTML = '<a target="_blank" href="https://gitdiagram.com/'+url+'"><img title="goto diagram" style="width: 28px;" src="https://raw.githubusercontent.com/daoyuly/gitdiagram_link/refs/heads/main/icon-128.png"></a>'
+        ul.appendChild(li); 
     }
-    var url = ul.innerText.replace(/\n/g, '')
 
-    var li = document.createElement('li')
-    li.innerHTML = '<a target="_blank" href="https://gitdiagram.com/'+url+'"><img title="goto diagram" style="width: 28px;" src="https://raw.githubusercontent.com/daoyuly/gitdiagram_link/refs/heads/main/icon-128.png"></a>'
-    ul.appendChild(li)
+    var contextRegion = document.querySelector('header .AppHeader-globalBar .AppHeader-globalBar-start .AppHeader-context-full nav context-region');
+    if (!contextRegion) {
+        return;
+    }
+    var url = contextRegion.innerText.replace(/\n/g, '')
+    var div = document.createElement('div');
+    div.innerHTML = '<a target="_blank" href="https://gitdiagram.com/'+url+'"><img title="goto diagram" style="width: 28px;" src="https://raw.githubusercontent.com/daoyuly/gitdiagram_link/refs/heads/main/icon-128.png"></a>'
+    contextRegion.appendChild(div);
 })()
